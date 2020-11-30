1953fc0d0882c4e3aa4f44b29718b3e17daf36f3
Emre Savas <e@emresavas.com>
2020-11-30 15:53:05 +0300

    add result output

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ echo "hostname: $3"
 #time=$(date)
 #echo "::set-output name=time::$time"
 #php -f /index.php $1 $2 $3
-curl -H"Authorization: cpanel $1:$2" "https://$3:2083/execute/VersionControl/retrieve?fields=name,type,branch,last_update"
+result = curl -H"Authorization: cpanel $1:$2" "https://$3:2083/execute/VersionControl/retrieve?fields=name,type,branch,last_update"
+echo "::set-output name=result::$result"
