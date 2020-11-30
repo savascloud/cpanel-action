05aaf1f698475a4c0dea81ace3c65ece23f4efa0
Emre Savas <e@emresavas.com>
2020-11-30 16:19:15 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ echo "hostname: $3"
 #echo "::set-output name=time::$time"
 #php -f /index.php $1 $2 $3
 result="$(curl --silent -H"Authorization: cpanel $1:$2" "https://$3:2083/execute/VersionControl/retrieve?fields=name,type,branch,last_update")"
-echo "::set-output name=result::$result"
+echo $result
